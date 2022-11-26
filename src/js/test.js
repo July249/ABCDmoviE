414aea6e41b8434fe1bfc323d4ba9d7080084b56
jihyunglee <jhlee7125@gmail.com>
2022-11-26 17:10:53 +0900

    style: 요청 obj 내 value 변경

--- src/js/test.js
+++ src/js/test.js
@@ -2,18 +2,20 @@ import createDB from '../../requestAPI.js';
 import findConfig from 'find-config';
 import dotenv from 'dotenv';
 
-const configs = dotenv.config({ path: findConfig('../.env') });
+const configs = dotenv.config({ path: findConfig('.env') });
 const id = configs.parsed.NAVER_CLIENT_ID;
 const key = configs.parsed.NAVER_CLIENT_SECRET;
+console.log(id, key);
 
 const obj = {
-  query: 'everything',
+  query: '블랙 팬서: 와칸다 포에버',
   display: 10,
   start: 1,
   genre: '',
   country: '',
-  yearfrom: '2021',
-  yearto: '2022',
+  yearfrom: '',
+  yearto: '',
 };
 
-createDB(obj, id, key);
+// createDB(obj, id, key);
+console.log();

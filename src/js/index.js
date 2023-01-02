7e415f224e352229b70b1cee4bbfda95c306bf7f
jihyunglee <jhlee7125@gmail.com>
2023-01-02 12:37:41 +0900

    feat: MovieData 클래스를 이용하여 데이터 요청하도록 변경

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,22 @@
-const getMovieImage = async () => {
-  const res = await fetch(
-    "https://raw.githubusercontent.com/Already-12-O-clock/ABCDmoviE/main/db/box_office_db.json"
-  );
-  const data = await res.json();
+import MovieData from "./getMovieData.js";
 
-  const boxOfficeMovieInfoArr = [];
-
-  data.items.map((item) => {
-    const resultObj = {};
-    resultObj["title"] = filterReg(item.title);
-    resultObj["image"] = item.image;
-    boxOfficeMovieInfoArr.push(resultObj);
-  });
-  return boxOfficeMovieInfoArr;
-};
-
-function filterReg(title) {
-  const regEx1 = /<b>/g;
-  const regEx2 = /<\/b>/g;
-  const filteredTit1 = title.replace(regEx1, "");
-  const filteredTit2 = filteredTit1.replace(regEx2, "");
-  return filteredTit2;
-}
+const fetchAPI = new MovieData();
+const data = await fetchAPI.getMovieData();
 
 const carousel = document.querySelector(".main-carousel");
 const imageContainer = carousel.querySelector(".item-list");
 const imageLis = carousel.querySelectorAll(".item-list > li");
 
-window.addEventListener("DOMContentLoaded", async () => {
-  const boxOfficeMovieInfoArr = await getMovieImage();
+window.addEventListener("load", async () => {
+  const boxOfficeMovieInfoArr = [];
+
+  data.items.map((item) => {
+    const resultObj = {};
+    resultObj["title"] = fetchAPI.filterReg(item.title);
+    resultObj["image"] = item.image;
+    boxOfficeMovieInfoArr.push(resultObj);
+  });
+
   const carouselImgDataArr = boxOfficeMovieInfoArr.slice(0, 5);
 
   carouselImgDataArr.forEach((imgInfoObj, idx) => {
@@ -51,10 +39,7 @@ const btns = carousel.querySelectorAll(".arr-btn");
 btns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     if (btn.classList.contains("left-btn")) {
-      imageContainer.insertBefore(
-        imageContainer.lastElementChild,
-        imageContainer.firstElementChild
-      );
+      imageContainer.insertBefore(imageContainer.lastElementChild, imageContainer.firstElementChild);
     } else if (btn.classList.contains("right-btn")) {
       imageContainer.appendChild(imageContainer.firstElementChild);
     }

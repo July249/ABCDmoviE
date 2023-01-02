af75aa32fb17e1e8b6cf6d69506f71b4fc43e42d
jihyunglee <jhlee7125@gmail.com>
2023-01-02 14:12:56 +0900

    feat: 이벤트가 발생한 img 요소의 src, alt 속성 정보를 이용하여 선택한 영화에 표시

--- src/js/movieSelect.js
+++ src/js/movieSelect.js
@@ -4,7 +4,7 @@ const fetchAPI = new MovieData();
 const data = await fetchAPI.getMovieData();
 
 const $selectMovieUl = document.querySelector(".select-ticket");
-const replaceImg = "https://unsplash-assets.imgix.net/empty-states/photos.png";
+const replaceImg = "https://t1.daumcdn.net/movie/c171adb6eaa6dff8bb70a1d07e8431e893237c08";
 
 window.addEventListener("load", async () => {
   const boxOfficeMovieInfoArr = [];
@@ -25,7 +25,29 @@ window.addEventListener("load", async () => {
       <img src=${resultObj.image || replaceImg} alt="${resultObj.title}" />
     `;
 
-    console.log($selectMovieLi);
     $selectMovieUl.appendChild($selectMovieLi);
   });
+
+  selectTicket();
 });
+
+function selectTicket() {
+  const $selectMovieLis = $selectMovieUl.querySelectorAll("li");
+  const $selectMovieContainer = document.querySelector(".buy-ticket");
+  const $selectMoviePoster = $selectMovieContainer.querySelector(".img-wrap");
+  const $selectMovieTitle = $selectMovieContainer.querySelector(".seleted-movie-tit");
+
+  $selectMovieLis.forEach((movie) => {
+    movie.addEventListener("click", (e) => {
+      const targetImg = e.currentTarget.children[0];
+      const imgUrl = targetImg.getAttribute("src");
+      const imgTitle = targetImg.getAttribute("alt");
+
+      $selectMoviePoster.innerHTML = `
+        <img src="${imgUrl}" alt="${imgTitle}" />
+      `;
+
+      $selectMovieTitle.textContent = imgTitle;
+    });
+  });
+}
